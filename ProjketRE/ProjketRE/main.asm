62b46af5319053b05d35e53009bb2f6d076a09a9
andreaciric <andreaciric23@gmail.com>
2020-07-25 22:48:58 +0200

    nemam pojma izbacuje neki breakpoint kad se debaguje, a meni je trenutno muka da ga gledam. btw može i ovo iz maina da se ubaci u proceduru.

--- ProjketRE/ProjketRE/main.asm
+++ ProjketRE/ProjketRE/main.asm
@@ -12,15 +12,71 @@ ExitProcess PROTO, dwExitCode:DWORD
 .data
 
      assign_array_indicator BYTE 0      ;//indikator pocetnog niza
+     colors BYTE 0h, 1h, 2h, 4h, 0Eh
 
 .data?
 
      arrayGame BYTE n dup(?)          ;//niz brojeva dodeljivanih u toku igre za random redosled kvadrata
      arraySetup BYTE n dup(?)          ;//inicijalna dodela brojeva bojama
+     xposition BYTE ?
+     yposition BYTE ?
 
      .code
           main PROC
-               INVOKE random_array, OFFSET ArrayGame, OFFSET ArraySetup, assign_array_indicator
+          INVOKE random_array, OFFSET arrayGame, OFFSET arraySetup, assign_array_indicator
+
+               mov edx, 0
+          draw:
+               mov ebx, OFFSET arrayGame
+               mov ecx, [ebx]
+               movzx eax, cl
+               mov ecx, eax
+               mov eax, OFFSET colors
+               mov esi, 0h
+               
+               .while (esi != ecx)
+                    inc esi
+                    inc eax
+               .endw
+
+               CMP edx, 0
+               JE first_square
+               CMP edx, 1
+               JE second_square
+               CMP edx, 2
+               JE third_square
+               CMP edx, 3
+               JE fourth_square
+
+          first_square:
+               mov xposition, 15
+               mov yposition, 1
+               mov edx, 1
+               JMP variables_set
+
+          second_square :
+               mov xposition, 35
+               mov yposition, 1
+               mov edx, 2
+               JMP variables_set
+
+          third_square :
+               mov xposition, 15
+               mov yposition, 10
+               mov edx, 3
+               JMP variables_set
+
+          fourth_square :
+               mov xposition, 35
+               mov yposition, 10
+               mov edx, 4
+                     
+          variables_set:
+               INVOKE draw_square, xposition, yposition, eax
+               CMP edx, 4
+               JNE draw
+               
+               
                INVOKE ExitProcess, 0
 
      main ENDP
